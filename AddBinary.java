845b33860b33eb2d83a052db5ac8968ef9ebe26c
Leon Ngai <40898479+leonngai@users.noreply.github.com>
2019-04-24 22:52:51 -0600

    second commit

--- AddBinary.java
+++ AddBinary.java
@@ -1,54 +1,19 @@
-
 class Solution {
     public String addBinary(String a, String b) {
         
-        int difference = Math.abs(a.length() - b.length()), carry = 0;
-        System.out.print(difference);
-        StringBuilder strA = new StringBuilder(a);
-        StringBuilder strB = new StringBuilder(b);
-        StringBuilder str = new StringBuilder();
-        
-        if (difference != 0) {
-            if (a.length() > b.length()) {
-                for(int i = 0; i < difference; i++)
-                    strB.insert(0,"0");
-            }
-            else {
-                 for(int i = 0; i < difference; i++)
-                    strA.insert(0,"0");
-            }
-        }
+        StringBuilder sum = new StringBuilder();
+        int aLength = a.length() - 1, bLength = b.length() - 1, carry = 0;
 
-        int index = strB.length();
-        System.out.print(index);
-        for(int i = 0; i < strA.length(); i++) {
+        while (aLength >= 0 || bLength >= 0 || carry == 1) {
             
-            if (carry + strA.charAt(index - 1) - 48 + strB.charAt(index - 1) - 48 == 3) {
-                str.insert(0,"1");
-                carry = 1;
-            }
-            else if (carry + strA.charAt(index - 1) - 48 + strB.charAt(index - 1) - 48 == 2) {
-                str.insert(0,"0");
-                carry = 1;
-            }
-            else if (carry + strA.charAt(index - 1) - 48 + strB.charAt(index - 1) - 48 == 1) {
-                str.insert(0,"1");
-                carry = 0;
-            }
-            else {
-            	str.insert(0, "0");
-            	carry = 0;
-            }
+            int aNum = aLength < 0 ? 0 : a.charAt(aLength--) - 48;
+            int bNum = bLength < 0 ? 0 : b.charAt(bLength--) - 48;
             
-           index--;
-            
-            if (i == strA.length() - 1 && carry == 1) {
-                str.insert(0,"1");
-                carry = 0;
-	            }
+            sum.insert(0, (carry + aNum + bNum) % 2);
+            carry = (carry + aNum + bNum) / 2;
             }
 
-        String sum = str.toString();
-        return sum;
+        String str = sum.toString();
+        return str;
     }
 }
