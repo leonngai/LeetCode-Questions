1e825e321e2cdb775e11ce91ddd1c79748e7c5b7
Leon Ngai <40898479+leonngai@users.noreply.github.com>
2019-04-26 09:10:31 -0600

    first commit

--- AllPathsFromSrcToTarget.java
+++ AllPathsFromSrcToTarget.java
@@ -1,1 +1,9 @@
-
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        helper(graph, 0, answer, path);
+        return answer;
+    }
+}
