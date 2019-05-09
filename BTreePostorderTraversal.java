ca25a6aa38e12c7feaef839e5e3b0fbd42a51b16
Leon Ngai <40898479+leonngai@users.noreply.github.com>
2019-05-09 09:22:04 -0600

    Update BTreePostorderTraversal.java

--- BTreePostorderTraversal.java
+++ BTreePostorderTraversal.java
@@ -1,4 +1,3 @@
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -8,3 +7,26 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> answer = new LinkedList<>();
+        if (root == null)
+            return answer;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        
+        while(root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                answer.addFirst(root.val);
+                root = root.right;
+            }
+            else {
+                TreeNode temp = stack.pop();
+                root = temp.left;
+            }
+        }
+        
+        return answer;
+    }
+}
