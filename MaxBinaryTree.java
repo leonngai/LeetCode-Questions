65681c8888233bab1ab1920bab68a35288bd3af2
Leon Ngai <40898479+leonngai@users.noreply.github.com>
2019-09-15 13:17:07 -0600

    my solution

--- MaxBinaryTree.java
+++ MaxBinaryTree.java
@@ -1,1 +1,47 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return helper(nums, 0, nums.length);
+    }
+    
+    public TreeNode helper(int[] nums, int l, int r) {
+        if (l == r) {
+            return null;
+        }
+        
+        int index = maxIndex(nums, l, r);
+        TreeNode root = new TreeNode(nums[index]);
+        
+        root.left = helper(nums, l, index);
+        root.right= helper(nums, index + 1, r);
+            
+        return root;
+    }
+    
+    public int maxIndex(int[] nums, int l, int r) {
+        int maxIndex = l;
+        for (int i = l; i < r; i++) {
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        
+        return maxIndex;
+    }
+    
+}
+
+/*
+find the index of the highest number in array
+then find index of highest number left and right of that index
+repeat
+*/
