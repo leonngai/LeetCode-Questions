71f08b7b2e7df758314f2f1fa2481f844708c8a4
Brittany Chung <brittanychung@Brittanys-MacBook-Air.local>
2019-09-19 20:22:20 -0600

    improved solution minimum add c++

--- MinimumAddParenthesis.cpp
+++ MinimumAddParenthesis.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int openUnmatched = 0;
-    int closedUnmatched = 0;
     
     int minAddToMakeValid(string S) {
-        for (int i = 0; i < S.length(); i++) {
-            if (S.at(i) == '(') 
+        int openUnmatched = 0, closedUnmatched = 0;
+        
+        for (char c : S) {
+            if (c == '(') 
                 openUnmatched++;
-            else if (S.at(i) == ')' && openUnmatched > 0)
+            else if (openUnmatched > 0)
                 openUnmatched--;
-            else if (S.at(i) == ')' && openUnmatched == 0)
+            else if (openUnmatched == 0)
                 closedUnmatched++;
         }
         
         return openUnmatched + closedUnmatched;
     }
+    
 };
