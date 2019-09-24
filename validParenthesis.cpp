be53e68a7a5c01fa6714c2f8ae18b658bd84c0ae
Leon Ngai <ngai.leon@live.ca>
2019-09-23 23:18:08 -0600

    my solution

---
+++ validParenthesis.cpp
@@ -0,0 +1,43 @@
+ass Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        
+        for (char c: s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            
+            else {
+                if (stack.empty())
+                    return false;
+                
+                if (c == ')') {
+                    if (stack.top() != '(') 
+                        return false;
+                        
+                    stack.pop();
+                }
+                    
+                else if (c == '}') {
+                    if (stack.top() != '{') 
+                        return false;
+                        
+                    stack.pop();
+                }
+                    
+                else if (c == ']') {
+                    if (stack.top() != '[') 
+                        return false;
+                        
+                    stack.pop();
+                }
+            }
+               
+          }
+         
+        return stack.empty();
+        }
+        
+        
+};
