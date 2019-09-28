bed497b443f31c7c467ec90d646902a0af1f0147
Leon Ngai <ngai.leon@live.ca>
2019-09-28 14:42:13 -0600

    My solution

---
+++ SwapNodesInPairs.java
@@ -0,0 +1,21 @@
+ass Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null||head.next == null) {
+            return head;
+        }
+
+        ListNode temp = head.next;
+        head.next = swapPairs(head.next.next);
+        temp.next = head;
+        return temp;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
