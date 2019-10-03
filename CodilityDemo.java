f0300486f89e22a53f0725779b46d55f114efa0a
Leon Ngai <ngai.leon@live.ca>
2019-10-02 21:37:10 -0600

    my solution

--- CodilityDemo.java
+++ CodilityDemo.java
@@ -1,10 +1,10 @@
-c int solution(int[] A) {
+public int solution(int[] A) {
         // write your code in Java SE 8
-        
+
         //segregate the negative and 0 values to the left side
         int startingindex = 0;
-        int ans = 0;
-        
+        int ans = 1;
+
         for (int i = 0; i < A.length; i++) {
             if (A[i] <= 0) {
                 int temp = A[i];
@@ -13,23 +13,27 @@ c int solution(int[] A) {
                 startingindex++;
             }
         }
-        
+
         int maxnumberallowed = A.length - startingindex;
         for (int i = startingindex; i < A.length; i++) {
-            if (A[i] > maxnumberallowed) 
+            if (A[i] > maxnumberallowed || A[i] == Integer.MIN_VALUE)
                 continue;
-                
+
             //A at the valued index would be set to negative
             A[A[i] + startingindex - 1] = Integer.MIN_VALUE;
         }
-        
-        
-        for (int i = startingindex; i <= A.length; i++) {
+
+
+        for (int i = startingindex; i < A.length; i++) {
             if (A[i] > 0) {
-                ans = i;
+                ans = i + 1;
                 break;
             }
         }
+
+        if (ans == 1 && startingindex == 0 && A[A.length - 1] == Integer.MIN_VALUE)
+            return A.length + 1;
+        
         
         return ans;
     }
