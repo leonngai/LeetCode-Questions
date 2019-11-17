794a3834bde4e854a21d9c99f5d8d109ceeca817
Leon Ngai <ngai@synopsys.com>
2019-11-16 23:21:57 -0700

    My solution:

---
+++ InvertBinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+    
+    public void helper(TreeNode root) {
+        if (root != null) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            
+            helper(root.left);
+            helper(root.right);
+        }
+    }
+}
